0344efc34cb288aa01847407bbad3d6c02944fff
Daniel <iamdanielsafronov@gmail.com>
2022-03-28 21:38:52 +0300

    Add initializer for the `HttpRequestHeader`

--- Sources/HttpClient/HttpRequestHeader.swift
+++ Sources/HttpClient/HttpRequestHeader.swift
@@ -11,4 +11,12 @@ import Foundation
 public struct HttpRequestHeader {
     let key: HttpRequestHeaderKey
     let value: String
+    
+    /// Initializer.
+    /// - parameter key: An instance of HttpRequestHeaderKey.
+    /// - parameter value: A header value.
+    public init(key: HttpRequestHeaderKey, value: String) {
+        self.key = key
+        self.value = value
+    }
 }
